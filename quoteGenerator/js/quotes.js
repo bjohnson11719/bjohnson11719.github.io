fc3d68db53626fa2158261f98626f5caa6ce9567
isomericmagic <bjohnson11719@gmail.com>
2025-04-10 07:05:55 -0500

    upload site files

---
+++ quoteGenerator/js/quotes.js
@@ -0,0 +1,81 @@
+//quotes array
+var quotes = [
+	{
+	  quote: "You can do anything but not everything.",
+	  source: "David Allen",
+	  citation: "Making It All Work",
+	  year: 2009
+	},
+	{
+	  quote: "Efforts to deepen your focus will struggle if you don’t simultaneously wean your mind from a dependence on distraction.",
+	  source: "Cal Newport",
+	  citation: "Deep Work: Rules for Focused Success in a Distracted World",
+	  year: 2016
+	},
+	{
+	  quote: "Well....Bye.",
+	  source: "Curly Bill Brocius",
+	  citation: "Tombstone"
+	},
+	{
+	  quote: "The more people we can find to agree with our side of the story, the more justified we will feel in believing that side of the story.",
+	  source: "The Arbinger Institute",
+	  citation: "Leadership and Self-Deception: Getting Out of the Box",
+	  year: 2000
+	},
+	{
+	  quote: "When we studied them, excellent performers were rarely well rounded. On the contrary, they were sharp.",
+	  source: "Donald O. Clifton",
+	  citation: "Now, Discover Your Strengths",
+	  year: 2001
+	},
+	{
+	  quote: "Get a good idea and stay with it. Dog it, and work at it until it's done right.",
+	  source: "Walt Disney"
+	},
+	{
+	  quote: "When someone does a small task beautifully, their whole environment is affected by it.",
+	  source: "Jerry Seinfeld"
+	},
+	{
+	  quote: "I don't like looking back. I'm always constantly looking forward. I'm not the one to sort of sit and cry over spilt milk. I'm too busy looking for the next cow.",
+	  source: "Gordon Ramsay"
+	},
+	{
+	  quote: "Pain don't hurt.",
+	  source: "Dalton",
+	  citation: "Road House"
+	},
+	{
+	  quote: "Do some good in the world for no other reason than wanting to be part of the solution.",
+	  source: "Cal Newport",
+	  citation: "How to Win at College: Surprising Secrets for Success from the Country's Top Students",
+	  year: 2005
+	},
+	{
+	  quote: "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.",
+	  source: "Bruce Lee"
+	},
+	{
+	  quote: "The object of life is not to be on the side of the majority, but to escape finding oneself in the ranks of the insane.",
+	  source: "Marcus Aurelius"
+	},
+	{
+	  quote: "By this method the discerning Kabbalist can decide that it is not wise to move against the tide, but wait for the flow to turn and place an impulse in the ebb that will bear fruit when the active phase begins again.",
+	  source: "Z.B.S. Halevi",
+	  citation: "The Work of the Kabbalist"
+	},
+	{
+	  quote: "If a house be divided against itself, that house cannot stand.",
+	  source: "Mark 3:25"
+	},
+	{
+	  quote: "It ain't how hard you hit...it's how hard you can get hit and keep moving forward.",
+	  source: "Rocky Balboa"
+	},
+	{
+	  quote: "Even though we are not responsible for the way we are and feel, we have to take responsibility for the way we act. Therefore we have to learn to discipline ourselves. And discipline rests on the ability to act in a manner that is contrary to our feelings when necessary.",
+	  source: "E.C. Whitmont",
+	  citation: "The Symbolic Quest"
+	}
+]
